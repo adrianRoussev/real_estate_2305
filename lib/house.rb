5639d9fffa48a8a4b398787dc045a905aac1ac1b
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-18 12:24:28 -0400

    update add_room class and initialize methods

--- lib/house.rb
+++ lib/house.rb
@@ -1,13 +1,16 @@
 class House
-    def initialize (price, address)
-        @price = price
-        @address= address
-        @rooms = []
-    end
-
-    def add_room(room)
-        @rooms <<room
-    end
-
-   
-end
+    attr_reader :price, :address, :rooms, :house_details, :room
+      def initialize (price, address, above_market_average = false)
+          @price = price
+          @address= address
+        @above_market_average = above_market_average
+          @rooms = []
+        @house_details = {}
+      end
+  
+      def add_room(room)
+         @room= Room.new(room.category, room.length, room.width)
+          @rooms <<@room
+        @house_details.merge!({@room.category =>   rooms.find_all{|x| x.category == @room.category}}) 
+      end
+    
